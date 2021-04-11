46c6893b1787426a1afda676bba91cd5e569f586
Soldy <4786022+Soldy@users.noreply.github.com>
2021-04-11 12:38:03 +0100

    error collction
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -107,6 +107,51 @@ const bigBang = function(){
      */
     let _int3rv4l ;
     /*
+     * @private
+     * @var {timeout}
+     */
+    let _3rr0r5 = {};
+    /*
+     * @param {Error}
+     * @private
+     * @return {boolean} // always false because the pipe
+    */
+    const _3rr0r0 = function(err){
+        _3rr0r0(err);
+        return undefined;
+    }
+    /*
+     * @param {Error}
+     * @private
+     * @return {boolean} // always false because the pipe
+    */
+    const _3rr0r = function(err){
+        let stamp = parseInt(Date.now()).toString();
+        if(typeof _3rr0r5[stamp] !== 'undefined')
+             return _3rr0r(err);
+        _3rr0r5[stamp]=err;
+        return false;
+    }
+    /*
+     * @private
+     * @return {array}
+    */
+    const _3rr0rList = function(){
+        let out = 0;
+        for (let i in _3rr0r5)
+            out++;
+        return out;
+    }
+    /*
+     * @param {string}
+     * @private
+     * @return {Error}
+    */
+    const _3rr0rGet = function(id){
+        if(typeof _3rr0r5[id] === 'undefined')
+            return new Error('Requested error not exist');
+    }
+    /*
      * @privat
      */
     const _endOfTheUniverse = function(){
@@ -132,7 +177,7 @@ const bigBang = function(){
      */
     const _baseGet = function(_name){
         if(typeof b4s3[name] === 'undefined') 
-            return false;
+            return _3rr0r0(new RangeError('base not exist'));
         return _b4s3[name];
     };
     /* Create a galaxy
@@ -158,7 +203,7 @@ const bigBang = function(){
     const _solarToGalaxy = function(galaxy, solar, planets){
         galaxy = _galaxyAdd(galaxy);
         if(typeof _g4l4x13s[galaxy][solar] !== 'undefined')
-            return false;
+            return _3rr0r0(new RangeError('solar already in this galaxy'));
         _g4l4x13s[galaxy][solar] = planets;
         return true;
     };
@@ -171,7 +216,7 @@ const bigBang = function(){
     const _solarFromGalaxy = function(galaxy, solar){
         _galaxyAdd(galaxy);
         if(typeof _g4l4x13s[galaxy][solar] !== 'undefined')
-            return undefined;
+            return _3rr0r0(new RangeError('solar not exist'));
         return _g4l4x13s[galaxy][solar];
     };
     /*  !!! JUST FOR MIGRATION !!!
@@ -182,7 +227,7 @@ const bigBang = function(){
     const _galaxyGet = function(galaxy){
         _galaxyAdd(galaxy);
         if(typeof _g4l4x13s[galaxy] === 'undefined') 
-            return undefined;
+            return _3rr0r0(new Error('galaxy is impossible'));
         return _g4l4x13s[galaxy];
     };
     /*
@@ -193,7 +238,7 @@ const bigBang = function(){
         if(_ev3ryth1ng.theUn1v3rse !== _hydr0g3n){
              _ev3ryth1ng.theUn1v3rse = _hydr0g3n;
              if(_h4pp3n3d === true)
-                 _v4l1d = false;
+                 _v4l1d = _3rr0r(new Error('Broken universe on '+_t1ck3d+'. tick' ));
         }
         // we can not stop in here :( the universe have to be fix himself
         _h4pp3n3d = true;
@@ -201,21 +246,24 @@ const bigBang = function(){
         _t1ck3d++;
         // temporaly code.
         _hydr0g3n.stat = {
-            'ad'       : (_0),
-            'happened' : (_h4pp3n3d),
-            'valid'    : (_v4l1d),
-            'ticked'   : (_t1ck3d),
-            'last'     : (_l4st)
+            'ad'       : parseInt(_0),
+            'happened' : (_h4pp3n3d === true),
+            'valid'    : (_v4l1d === true),
+            'ticked'   : parseInt(_t1ck3d),
+            'last'     : parseInt(_l4st)
 
         };
         _hydr0g3n.controls = {
-            'baseAdd'          : _baseAdd,
-            'basGet'           : _baseGet,
-            'galaxyGet'        : _galaxyGet,
-            'galaxyAdd'        : _galaxyAdd,
-            'solarTogalaxy'    : _solarToGalaxy,
-            'solarFromGalaxy'  : _solarFromGalaxy,
-            'exit'             : _endOfTheUniverse
+            'baseAdd'         : function(name,base){return _baseAdd(name,base);},
+            'basGet'          : function(name){return _baseGet(name);},
+            'galaxyGet'       : function(galaxy){return _galaxyGet(galaxy);},
+            'galaxyAdd'       : function(name){return _galaxyAdd(name);},
+            'error'           : function(error){return _3rr0r(error);},
+            'errorGet'        : function(stamp){return _3rr0rGet(stamp);},
+            'errorList'       : function(){return _3rr0rList();},
+            'solarTogalaxy'   : function(galaxy,solar,planets){return _solarToGalaxy(galaxy,solar,palnets);},
+            'solarFromGalaxy' : function(galaxy,solar){return _solarFromGalaxy(galaxy,solar);},
+            'exit'            : function(){return _endOfTheUniverse();}
         };
         _int3rv4l = setTimeout(
             _boo000om, 
