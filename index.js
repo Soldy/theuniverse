825dbbd040e6f47252e511cdf9d372efa8b06236
Soldy <4786022+Soldy@users.noreply.github.com>
2021-06-15 20:29:53 +0100

    secret ichy
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 if(
-     (
-         (typeof global === 'undefined') ||
+    (
+        (typeof global === 'undefined') ||
          (typeof global.theUn1v3rse === 'undefined')
-     )&&(
-         (typeof window === 'undefined') ||
+    )&&(
+        (typeof window === 'undefined') ||
          (typeof window.theUn1v3rse === 'undefined')
-     )
+    )
 )
-     new (require('./universe.js')).base();
+    new (require('./universe.js')).base();
